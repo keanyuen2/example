e7433eaca9dae365efa081aa43aadceea4c94908
keanyuen2 <keanyuen.kau2@gmail.com>
2023-02-20 23:51:33 +0800

    Initial commit

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,7 +1,7 @@
-<x-app-layout>
+<div>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{ __('Dashboard') }}
+            {{ __('Counter') }}
         </h2>
     </x-slot>
 
@@ -9,9 +9,10 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <input class="w-20 rounded-md h-7" type="number" value="{{ $count }}" disabled />
+                    <button class="w-20 text-white bg-blue-400 border-2 border-blue-500 rounded-md" wire:click="increment">+</button>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+</div>
