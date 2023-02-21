2db3770a93368cfb2e08d996f1cb69795480880e
keanyuen <keanyuen.kau@marvelmedia.com>
2023-02-21 12:12:46 +0800

    Initial commit

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -9,8 +9,8 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <input class="w-20 rounded-md h-7" type="number" value="{{ $count }}" disabled />
-                    <button class="w-20 text-white bg-blue-400 border-2 border-blue-500 rounded-md" wire:click="increment">+</button>
+                    <x-input.textbox type="number" value="{{ $count }}" disabled></x-input.textbox>
+                    <x-input.primary-button :class="'py-2 px-4'" :text="'+'" wire:click="increment"></x-input.primary-button>
                 </div>
             </div>
         </div>
