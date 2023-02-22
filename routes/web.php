93933c52b28ee00d53279e774d5768ed90c3df4d
keanyuen2 <keanyuen.kau2@gmail.com>
2023-02-22 08:29:06 +0800

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/home', \App\Http\Livewire\Home::class)->name('home');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/home', \App\Http\Livewire\Home::class)->name('home');
 });
 
 require __DIR__.'/auth.php';
